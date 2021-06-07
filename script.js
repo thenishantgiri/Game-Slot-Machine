c4ea6710efe418d78eb4347534b1986deebf7d8f
Nishant Kumar Giri <nishantkrgiri@gmail.com>
2021-06-07 18:56:56 +0530

    Project : Game Slot Machine - base

---
+++ script.js
@@ -0,0 +1,31 @@
+let value1 = document.getElementById("value1");
+let value2 = document.getElementById("value2");
+let value3 = document.getElementById("value3");
+
+let inpSpeed = document.getElementById("inpSpeed");
+
+let values = ["🎰", "😃", "🤑", "👻", "🤡", "👾", "🤖"];
+
+function getRandomValue() {
+  return values[Math.floor(Math.random() * 7)];
+}
+
+let animationId;
+
+function updateAnimation(newSpeed) {
+  if (animationId) clearInterval(animationId);
+
+  animationId = setInterval(() => {
+    value1.innerText = getRandomValue();
+    value2.innerText = getRandomValue();
+    value3.innerText = getRandomValue();
+  }, 1000 / newSpeed);
+}
+
+inpSpeed.onchange = (val) => {
+  //   console.log("value changed :", val.target.value);
+  //document.documentElement => this is "root" of css
+
+  document.documentElement.style.setProperty("--speed", val.target.value);
+  updateAnimation(val.target.value);
+};
